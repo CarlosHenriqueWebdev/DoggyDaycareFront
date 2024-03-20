96d69cc38f52bd0e2a2779b80e728522d5e5b165
Carlos Henrique <157331695+CarlosHenriqueWebdev@users.noreply.github.com>
2024-03-20 00:59:15 -0300

    Update Map.js

--- src/components/page-content/map/Map.js
+++ src/components/page-content/map/Map.js
@@ -167,8 +167,8 @@ const Map = () => {
                 </button>
 
                 <TileLayer
-                  attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                  url="https://tiles.stadiamaps.com/tiles/stamen_terrain/{z}/{x}/{y}{r}.png"
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
 
                 <MapEvent />
